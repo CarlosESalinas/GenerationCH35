1d304c2ca8487eba40c4ed35453e10e3b631d424
CarlosESalinas <salinas_carlos@ciencias.unam.mx>
2023-12-14 16:56:25 -0600

    (+) Se actuliza el archivo operadores.js

--- JavaScript/operadores.js
+++ JavaScript/operadores.js
@@ -41,4 +41,25 @@ function compararPalabras(palabra1, palabra2){
     }
 }
 
-console.log(compararPalabras());
+// console.log(compararPalabras());
+
+/*
+Ejericio 3
+Escribe un programa para realizar hotcakes,
+el programa debe preguntar al usuario si tiene
+los ingredientes necesarios para realizar los hotcakes.
+*/
+
+function hacerHotcakes(){
+    let leche = prompt("¿Tienes leche? (si/no)");
+    let huevos = prompt("¿Tienes huevos? (si/no)");
+    let harina = prompt("¿Tienes harina? (si/no)");
+    let mantequilla = prompt("¿Tienes mantequilla? (si/no)");
+    let azucar = prompt("¿Tienes azúcar? (si/no)");
+
+    if (leche === "si" && huevos === "si" && harina === "si" && mantequilla === "si" && azucar === "si"){
+        console.log("Puedes hacer hotcakes");
+    } else {
+        console.log("No puedes hacer hotcakes");
+    }
+}
